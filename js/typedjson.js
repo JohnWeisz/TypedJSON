c033fd06ae8da73a6e8c347e88aca112f0bf6140
Michał Dębski <debski.mi.zd@gmail.com>
2019-03-14 23:53:28 +0100

    1.3.0

--- js/typedjson.js
+++ js/typedjson.js
@@ -1,4 +1,4 @@
-// [typedjson]  Version: 1.2.3 - 2019-03-14  
+// [typedjson]  Version: 1.3.0 - 2019-03-14  
  (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
