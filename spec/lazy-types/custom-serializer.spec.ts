a8c93fe877d5fb7772fbd3e6721eeb16f59242a0
amoscatelli <alessandro.moscatelli@live.com>
2021-03-07 13:09:22 +0100

    Update custom-serializer.spec.ts
    code cleaning

--- spec/lazy-types/custom-serializer.spec.ts
+++ spec/lazy-types/custom-serializer.spec.ts
@@ -5,7 +5,6 @@ describe('lazy, custom array member serializer', () => {
     @jsonObject
     class Obj {
         @jsonArrayMember(() => Number, {serializer: (values: Array<number>) => values.join(',')})
-
         nums: Array<number>;
 
         @jsonMember
